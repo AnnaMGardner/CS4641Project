daba08a8b74608028c465771f234151842412995
AnnaMGardner <70662115+AnnaMGardner@users.noreply.github.com>
2022-07-05 14:16:34 -0400

    Update 3d_brain.js

--- javascript/3d_brain.js
+++ javascript/3d_brain.js
@@ -1,2 +1,2 @@
-var loadedModel = taccgl.objFile().read( '/objtest/roundcube/roundcube1.obj');
+var loadedModel = taccgl.objFile().read( '/3d_brain.obj');
 taccgl.actor("braindiv", loadedModel.scene()) . rotateMiddle(1,1,1) . duration(10) .start();
