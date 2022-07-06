269b3edd989b52cd333c7329fce850cd5202ac39
zliu387 <105752891+zliu387@users.noreply.github.com>
2022-07-06 17:08:22 -0400

    Update largeDimensionClusterPlot.py

--- largeDimensionClusterPlot.py
+++ largeDimensionClusterPlot.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 exampleData2D = pd.read_csv("/Users/lukez/Desktop/CS4641/Untitled Folder/processed_data/UnbTsne2D.csv")
@@ -40,7 +42,7 @@ def largeDimensionClusterPlot2D(data2D, index):
     return None
 
 
-def largeDimensionClusterPlot3D(data3D, index):
+def largeDimensionClusterPlot3D(data3D, index, rotate = False):
     """
     :param data2D: pandas dataframe processed 2D data in the follow format:
             feature 1   feature 2   feature 3   stroke
@@ -67,15 +69,23 @@ def largeDimensionClusterPlot3D(data3D, index):
     clusters = [clusterGroup.get_group(x) for x in clusterGroup.groups]
     clusterIdx = 0
     for cluster in clusters:
+        # print("In cluster ", clusterIdx)
+        # print(cluster['labels'].value_counts())
+
         positive = cluster.loc[cluster['labels'] == 1]
         negative = cluster.loc[cluster['labels'] == 0]
         ax.scatter3D(positive[feature1], positive[feature2], positive[feature3], marker='.', c=colors[clusterIdx])
         ax.scatter3D(negative[feature1], negative[feature2], negative[feature3], marker='x', c=colors[clusterIdx])
         clusterIdx += 1
     # plt.legend()
+    if (rotate == True):
+        for angle in range(0, 360):
+            ax.view_init(30, angle)
+            plt.pause(.001)
     plt.show()
     return None
 
 
-largeDimensionClusterPlot2D(exampleData2D, exampleIndex)
-largeDimensionClusterPlot3D(exampleData3D, exampleIndex)
+# largeDimensionClusterPlot2D(exampleData2D, exampleIndex)
+largeDimensionClusterPlot3D(exampleData3D, exampleIndex, rotate=True)
+
