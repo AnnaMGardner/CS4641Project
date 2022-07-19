560377fa507db255447ec4d9278598765362aeed
AnnaMGardner <70662115+AnnaMGardner@users.noreply.github.com>
2022-07-19 16:36:54 -0400

    Update form.js

--- javascript/form.js
+++ javascript/form.js
@@ -8,13 +8,10 @@ function ajaxpost () {
   // (B) AJAX REQUEST
   // (B1) POST DATA TO SERVER, RETURN RESPONSE AS TEXT
   fetch("https://vyfeovv08c.execute-api.us-east-2.amazonaws.com/beta", { method:"POST", body:json})
-  .then(res=>res.text())
- 
-  // (B2) SHOW MESSAGE ON SERVER RESPONSE
   .then((response) => {
-    console.log(response);
-    if (response == "Low Risk") { document.getElementById("strokeRiskResult").appendChild(<iframe src="https://giphy.com/embed/ZgCM9TWQ5lL8zpc4G2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZgCM9TWQ5lL8zpc4G2">via GIPHY</a></p>) ;}
-    else { document.getElementById("strokeRiskResult").appendChild(<iframe src="https://giphy.com/embed/ZgCM9TWQ5lL8zpc4G2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZgCM9TWQ5lL8zpc4G2">via GIPHY</a></p>) ; }
+    console.log(response.text());
+    if (response == "Low Risk") { document.getElementById("strokeRiskResult").appendChild('<iframe src="https://giphy.com/embed/ZgCM9TWQ5lL8zpc4G2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZgCM9TWQ5lL8zpc4G2">via GIPHY</a></p>') ;}
+    else { document.getElementById("strokeRiskResult").appendChild('<iframe src="https://giphy.com/embed/ZgCM9TWQ5lL8zpc4G2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZgCM9TWQ5lL8zpc4G2">via GIPHY</a></p>') ; }
   })
  
   // (B3) OPTIONAL - HANDLE FETCH ERROR
