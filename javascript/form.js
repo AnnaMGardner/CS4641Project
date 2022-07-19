6046de5fbb81607543a5d725fff817e1abf83700
AnnaMGardner <70662115+AnnaMGardner@users.noreply.github.com>
2022-07-19 14:13:55 -0400

    Create form.js

---
+++ javascript/form.js
@@ -0,0 +1,23 @@
+function ajaxpost () {
+  // (A) GET FORM DATA
+  var form = document.getElementById("myForm");
+  var data = new FormData(form);
+ 
+  // (B) AJAX REQUEST
+  // (B1) POST DATA TO SERVER, RETURN RESPONSE AS TEXT
+  fetch("https://vyfeovv08c.execute-api.us-east-2.amazonaws.com", { method:"POST", body:data })
+  .then(res=>res.text())
+ 
+  // (B2) SHOW MESSAGE ON SERVER RESPONSE
+  .then((response) => {
+    console.log(response);
+    if (response == "Low Risk") { alert("SUCCESSFUL!"); }
+    else { alert("FAILURE!"); }
+  })
+ 
+  // (B3) OPTIONAL - HANDLE FETCH ERROR
+  .catch((err) => { console.error(err); });
+ 
+  // (C) PREVENT FORM SUBMIT
+  return false;
+}
