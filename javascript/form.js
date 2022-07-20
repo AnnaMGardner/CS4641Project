4eeffaaced729b27c29e2fb51ed26396c7846d8e
AnnaMGardner <70662115+AnnaMGardner@users.noreply.github.com>
2022-07-20 19:00:50 -0400

    Update form.js

--- javascript/form.js
+++ javascript/form.js
@@ -2,16 +2,32 @@ function ajaxpost () {
   // (A) GET FORM DATA
   var form = document.getElementById("myForm");
   var object = {};
-  FormData.forEach((value, key) => object[key] = value);
-  var json = JSON.stringify(object);
+  var link = "https://giphy.com/embed/ZgCM9TWQ5lL8zpc4G2";
+  var iframe = document.createElement('iframe');
+  iframe.frameBorder=0;
+  iframe.width="300px";
+  iframe.height="250px";
+  iframe.id="randomid";
+  iframe.setAttribute("src", link);
+  var formData = new FormData(document.querySelector('form'))
+  var formDataObject = Object.fromEntries(formData.entries());
+  // Format the plain form data as JSON
+  var formDataJsonString = JSON.stringify(formDataObject);
+  console.log(formDataJsonString)
+  // var formData = new FormData(document.querySelector('form'))
+  // formData.forEach((value, key) => object[key] = value);
+  // var json = JSON.stringify(object);
+  // console.log(json)
  
   // (B) AJAX REQUEST
   // (B1) POST DATA TO SERVER, RETURN RESPONSE AS TEXT
-  fetch("https://vyfeovv08c.execute-api.us-east-2.amazonaws.com/beta", { method:"POST", body:json})
+  fetch("https://vyfeovv08c.execute-api.us-east-2.amazonaws.com/beta", { method:"POST", headers: {
+    'Content-Type': 'application/json;charset=utf-8'
+  }, body:formDataJsonString})
   .then((response) => {
-    console.log(response.text());
-    if (response == "Low Risk") { document.getElementById("strokeRiskResult").appendChild('<iframe src="https://giphy.com/embed/ZgCM9TWQ5lL8zpc4G2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZgCM9TWQ5lL8zpc4G2">via GIPHY</a></p>') ;}
-    else { document.getElementById("strokeRiskResult").appendChild('<iframe src="https://giphy.com/embed/ZgCM9TWQ5lL8zpc4G2" width="480" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/ZgCM9TWQ5lL8zpc4G2">via GIPHY</a></p>') ; }
+    console.log("got here")
+    if (response.text() == "Low Risk") { document.getElementById("strokeRiskResult").appendChild(iframe) ;}
+    else { document.getElementById("strokeRiskResult").appendChild(iframe) ; }
   })
  
   // (B3) OPTIONAL - HANDLE FETCH ERROR
